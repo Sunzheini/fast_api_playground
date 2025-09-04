89998c7088b182ab6095ce4ca19f200b60ae3ebd
Sunzheini <daniel_zorov@abv.bg>
2025-09-04 14:29:41 +0300

    feat: add health check endpoint; implement /healthy route for service status verification

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ from routers import auth, users
 app = FastAPI()                                 # create a FastAPI instance
 app.include_router(auth.router)                 # include the authentication router
 app.include_router(users.router)                # include the users router
+
+
+@app.get("/healthy")
+async def health_check():
+    return {"status": "ok"}
